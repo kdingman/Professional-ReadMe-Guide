16b77341d4629c4ad62ac0c6b336fc97b5fedca8
Kellly <kcdonlan@yahoo.com>
2022-01-30 12:09:50 -0500

    user questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+const promptUserInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your GitHub username.');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter a valid email address. (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a valid email address.');
+                }
+            }
+
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
